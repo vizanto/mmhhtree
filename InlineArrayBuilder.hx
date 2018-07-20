a88904d85d65e84571c4d19b82f03ff643e7c01c
Danny Wilson <danny.wilson@laerdal.com>
2018-07-20 17:49:12 +0200

    InlineArray: Use getter return type. Support FixedLength size parameter.

--- InlineArrayBuilder.hx
+++ InlineArrayBuilder.hx
@@ -15,23 +15,27 @@ class InlineArrayBuilder
 #if macro
     static var typeMap : Map<Int,ComplexType> = new Map();
 
-    static private function buildClass(name, type_T, itemSize:Int)
+    static private function buildClass(name, itemSize:Int)
     {
-        var i = macro this.offset + $v{itemSize} * index;
+        var i = macro offset + $v{itemSize} * index;
         var getter = switch itemSize {
-            case 1: macro (this.buffer).get      ($i);
-            case 2: macro (this.buffer).getInt16 ($i);
-            case 4: macro (this.buffer).getInt32 ($i);
-            case 8: macro (this.buffer).getInt64 ($i);
-            case s: macro (this.buffer).sub      ($i, $v{s});
+            // (buffer) wrapped in parens is done here to help Haxe with inline new:
+            case 1: macro (buffer).get      ($i);
+            case 2: macro (buffer).getInt16 ($i);
+            case 4: macro (buffer).getInt32 ($i);
+            case 8: macro (buffer).getInt64 ($i);
+            case s: macro new ReadOnlyMemory(buffer).sub($i, $v{s});
         }
+        var fakeGetCall = macro { var offset = 0, buffer:Bytes = null, index = 0; $getter; };
+        var type_T = TypeTools.toComplexType(Context.typeof(fakeGetCall));
+
         var sizeOf = macro this.length * $v{itemSize};
         var self : TypePath = {name:name, pack:[]};
         var def = macro class $name extends InlineArray.ReadOnlyBufferSlice implements InlineArray.InlineArrayAPI<$type_T>
         {
             inline public function new (buffer, offset, length) super(buffer, offset, length);
 
-            @:pure inline public function get    (index)      return $getter;
+            @:pure inline public function get    (index:Int)  return $getter;
             @:pure inline public function stride ()           return $v{itemSize};
             @:pure inline public function sizeOf ()           return $sizeOf;
             @:pure inline public function slice  (index, end) return new $self(this.buffer, $i, end);
--- ReadOnlyMemory.hx
+++ ReadOnlyMemory.hx
@@ -4,10 +4,10 @@ import io.Bytes;
 @:forward(get, getInt32, getInt64)
 abstract ReadOnlyMemory (Bytes) to Bytes
 {
-    inline public function new(buffer : Bytes)
+    inline public function new (buffer : Bytes)
         // this = #if java buffer.asReadOnlyBuffer().order(java.nio.ByteOrder.LITTLE_ENDIAN) #else buffer #end;
         this = buffer;
 
-    inline public function sub(index, length)
+    inline public function sub (index:Int, length:Int)
         return new ReadOnlyMemory(this.sub(index, length));
 }
